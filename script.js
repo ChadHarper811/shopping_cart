53de53ac60f8fa344f32da6190449e03649fffd7
Chad Harper <chadharper811@gmail.com>
2024-08-03 12:50:44 -0500

    finished event handler for show/hide cart

--- script.js
+++ script.js
@@ -139,4 +139,8 @@ const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
         })
     }
 );
-cartBtn.addEventListener("click", () => {})
+cartBtn.addEventListener("click", () => {
+    isCartShowing = !isCartShowing;
+    showHideCartSpan.textContent = isCartShowing ? "Hide" : "Show";
+    cartContainer.style.display = isCartShowing ? "block" : "none";
+});
